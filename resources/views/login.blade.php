cd53dff69ea6c6daa65fc8f6158c04c4a3eb8fab
Leonardo Henrique <leonardohenriquesoarasalvez784@gmail.com>
2025-07-20 10:29:22 -0300

    feat: add login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -13,7 +13,9 @@
                     com recomendações personalizadas e avaliações de diferentes gêneros.
                 </p>
             </div>
-            <x-button linkto="criar-conta" variant="gradient" class="me-2 mb-2">Começar agora mesmo!</x-button>
+            <a href="#"
+                class="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Começar
+                agora mesmo!</a>
         </div>
     </section>
 @endsection
